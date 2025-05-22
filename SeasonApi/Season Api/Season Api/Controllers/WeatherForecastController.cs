df1d49522c74ba313640851f72814dd8c24cb9b0
Wodjess <74964006+Wodjess@users.noreply.github.com>
2025-05-22 19:13:47 +0300

    SeasonAPI using ASP.NET with .NET Core 8.0
    Simple API that's waiting a http request and returning season

---
+++ SeasonApi/Season Api/Season Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Season_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+        
+        
+    }
+}
---
+++ SeasonApi/Season Api/TestApi.cs
@@ -0,0 +1,5 @@
+namespace Season_Api
+{
+    
+
+}
