43608f63d6899b0a80c4796e3087a798b5c2ea41
havers0n <danypetrov2002@gmail.com>
2025-06-28 04:07:56 +0300

    fix: resolve dependency conflict for vite-plugin-static-copy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/images',
+          dest: ''
+        }
+      ]
+    })
   ]
 }) 
